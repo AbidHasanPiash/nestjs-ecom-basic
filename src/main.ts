4f243437ed5fcf1247edb8bd629825182be61170
AbidHasanPiash <meghpiash@gmail.com>
2025-02-01 23:29:18 +0600

    set global API prefix and enable validation pipes in main application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  
-  console.log('Database Host:', configService.get<string>('BD_HOST')); // Should print 'localhost'
+  console.log('Database Host:', configService.get<string>('BD_HOST'));
 
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
